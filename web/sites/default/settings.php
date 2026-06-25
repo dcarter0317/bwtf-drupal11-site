1b931c90c47dbc7c0469c8bf0b7ac93b52ca5486
dcarter0317 <dcarter0317@yahoo.com>
2026-06-25 16:37:48 -0400

    Configure Drupal 11 local development and export site configuration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -878,3 +878,7 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
